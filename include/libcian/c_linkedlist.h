00b0287cf2959bd555f07dc6a00810dab4c93579
Cian O'Sullivan <inventer100@gmail.com>
2020-07-15 18:11:42 +0100

    Add linkedlist documentation

--- include/libcian/c_linkedlist.h
+++ include/libcian/c_linkedlist.h
@@ -1,52 +1,104 @@
 #ifndef C_LINKEDLIST_H
 #define C_LINKEDLIST_H
-#include <stddef.h> // NULL
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdlib.h>   // malloc(), NULL
+#include <stdio.h>    // fprintf(), printf()
 
+/**
+A single item in the list containing the data and a pointer to the next node.
+ */
 typedef struct node {
     int data;
     struct node * next;
 } node;
 
+/**
+Make the linked list structure containing the head node and number of items in
+the list.
+ */
 typedef struct c_ll {
     struct node* head;
     int count;
 } c_ll;
 
-c_ll* list;
-//int count;       // Keeps track of number of items in list
+/**
+Make a new linked list and return the list.
 
+@returns a pointer to the linked list
+ */
 c_ll* c_ll_init();
+
+/**
+Print the contents of the linked list.
+
+@param list the list to print
+ */
 void c_ll_print(c_ll* list);
 
 /**
 Adds data to end of linked list.
+
+@param list the list to add the item to
+@param data the data to add to the list
  */
 void c_ll_add(c_ll* list, int data);
 
 /**
 Adds data to start of linked list.
+
+@param list the list to add the item to
+@param data the data to add to the list
+
  */
 void c_ll_add_start(c_ll* list, int data);
 
 /**
 Remove data at start of linked list.
+
+@param list the list to add the item to
  */
 void c_ll_remove_front(c_ll* list);
 
 /**
 Remove data from end of linked list.
+
+@param list the list to remove the item from
  */
 void c_ll_remove_end(c_ll* list);
 
 /**
 Remove data at the given index.
+
+@param list the list to remove the item from
+@param index the location of the element to be removed
+
  */
 void c_ll_remove_at(c_ll* list, int index);
 
+/**
+Get the node at the given index.
+
+@returns the node in the list at the given index
+@param list the list to get the node from
+@param index the location in the list of the desired node
+ */
 node* c_ll_get(c_ll* list, int index);
 
+/**
+Get the number of items in the given list.
+
+@returns the number of items in the list
+@param list the list to return the size of
+ */
 int c_ll_size(c_ll* list);
+
+/**
+Reset the current position of the list
+ */
+void c_ll_first();
+
+/**
+Get the next node in the list.
+ */
+node* c_ll_next();
 
 #endif
--- src/c_linkedlist.c
+++ src/c_linkedlist.c
@@ -1,11 +1,13 @@
 #include "c_linkedlist.h"
 
 c_ll* c_ll_init() {
-    list = (c_ll*) malloc(sizeof(c_ll));
+    // Initialise a new list and set number of items to 0
+    c_ll* list = (c_ll*) malloc(sizeof(c_ll));
     list->count = 0;
 
+    // Make the head node (head node is treated as the pointer to the list)
     list->head = (node*) malloc(sizeof(node));
-    list->head->data = 1;
+    list->head->data = -1;
     list->head->next = NULL;
 
     return list;
@@ -14,59 +16,71 @@ c_ll* c_ll_init() {
 void c_ll_add(c_ll* list, int data) {
     node* current = list->head;
 
+    // Move to the end of the list
     while (current->next != NULL) {
         current = current->next;
     }
 
+    // Add a new node with the given data to the list
     current->next = (node*) malloc(sizeof(node));
     current->next->data = data;
     current->next->next = NULL;
+
     list->count += 1;
 }
 
 void c_ll_add_start(c_ll* list, int data) {
     node* current = list->head;
 
+    // Make the new node and rejig the pointers
     node* newNode = (node*) malloc(sizeof(node));
     newNode->data = data;
     newNode->next = current->next;
     current->next = newNode;
+
     list->count += 1;
 }
 
 
 void c_ll_remove_front(c_ll* list) {
-    node* current = list->head->next; // Skip first node
-
+    // Skip if list is empty
     if (list->count == 0) {
         fprintf(stderr, "Can't remove element from empty list.\n");
         return;
     }
+    node* current = list->head->next; // Always skip head node
 
+    //Rejig pointers and free floating pointer
     list->head->next = current->next;
     free(current);
+
     list->count -= 1;
 }
 
 void c_ll_remove_end(c_ll* list) {
+    // Skip if list is empty
     if (list->count == 0) {
         fprintf(stderr, "Can't remove element from empty list.\n");
         return;
     }
 
+    // Skip to second last element in list
     node* current = list->head;
-
     while (current->next->next != NULL) {
         current = current->next;
     }
+
+    // Reassign next element to null
     free(current->next);
     current->next = NULL;
+
     list->count -= 1;
 }
 
 node* c_ll_get(c_ll* list, int index) {
     node* current = list->head->next; // Skip head node
 
+    // Iterate through list until index
     for (int i = 0; i < index; i++) {
         if (current->next == NULL) {
             fprintf(stderr, "IndexError: Can't remove element at index from linked list.\n");
@@ -81,6 +95,7 @@ node* c_ll_get(c_ll* list, int index) {
 void c_ll_remove_at(c_ll* list, int index) {
     node* current = list->head->next; // Skip head node
 
+    // Run specific remove methods if applicable
     if (index == 0) {
         c_ll_remove_front(list);
         return;
@@ -89,6 +104,7 @@ void c_ll_remove_at(c_ll* list, int index) {
         return;
     }
 
+    // Iterate through list until the index
     for (int i = 0; i < index-1; i++) {
         if (current->next == NULL) {
             fprintf(stderr, "IndexError: Can't remove element at index from linked list.\n");
@@ -97,19 +113,21 @@ void c_ll_remove_at(c_ll* list, int index) {
         current = current->next;
     }
 
+    // Remove node and rejig pointers
     node* temp = current->next;
     current->next = temp->next;
     free(temp);
+
     list->count -= 1;
 }
 
 void c_ll_print(c_ll* list) {
-    int i = 0;
     node* current = list->head->next;    // Always skip head node
+
+    // Iterate through list and print the data
     while (current != NULL) {
-        printf("Element %d: %d\n", i, current->data);
+        printf("%d\n", current->data);
         current = current->next;
-        i++;
     }
 }
 
